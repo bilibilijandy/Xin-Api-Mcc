e0783e37caa0c43352cf38ddadd38768da3cb9e6
bilibilijandy <141329748+bilibilijandy@users.noreply.github.com>
2025-08-17 15:46:50 +0800

    a

--- MinecraftClient/ChatBots/UploadBot.cs
+++ MinecraftClient/ChatBots/UploadBot.cs
@@ -5,14 +5,16 @@ using System.Text.Json;
 using System.Net.Http;
 using MinecraftClient.Scripting;
 
-namespace MinecraftClient.ChatBots.UploadBot;
-
-
+namespace MinecraftClient.ChatBots
+{
+    /// <summary>
+    /// Example of message receiving.
+    /// </summary>
     public class UploadBot : ChatBot
     {
         private Timer timer;
         private HttpClient httpClient = new HttpClient();
-
+        
         // 发送POST请求
         private async Task<string> SendPostRequest(string url, string jsonContent)
         {
@@ -53,7 +55,7 @@ namespace MinecraftClient.ChatBots.UploadBot;
                 LogToConsole($"执行错误: {e.Message}");
             }
         }
-
+        
         public override void OnUnload()
         {
             // 清理资源
@@ -61,3 +63,4 @@ namespace MinecraftClient.ChatBots.UploadBot;
             httpClient?.Dispose();
         }
     }
+}
